0aa03c0af5e0f3c97416d0b8fa5912cd12843e0a
its-snr <learning.nirmalraj@gmail.com>
2024-09-01 09:20:49 +0530

    Time: 159 ms (100%), Space: 123 MB (100%) - LeetHub

---
+++ 3495-k-th-nearest-obstacle-queries/3495-k-th-nearest-obstacle-queries.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int[] resultsArray(int[][] queries, int k) {
+
+        int n=queries.length;
+        int[] ans=new int[n];
+        PriorityQueue<Integer>tree=new PriorityQueue<Integer>(
+        Comparator.reverseOrder());
+        PriorityQueue<Integer>min=new PriorityQueue<Integer>();
+        ArrayList<Integer> arr=new ArrayList<Integer>();
+        int t=1;
+        for(int i=0;i<n;i++){
+            int a=Math.abs(queries[i][0]);
+            int b=Math.abs(queries[i][1]);
+            // if(t>=k){
+            //     ans[i]=a+b;
+            // if(tree.contains(a+b)){
+            //     // ArrayList<Integer> arr=new ArrayList<Integer>(tree);
+            //     if(arr.size()>=k){
+            //         int temp=arr.get(k-1);
+            //         if(temp==)
+            //         ans[i]=arr.get(k-1);
+            //     }
+            //     else ans[i]=-1;
+
+            // }
+            // else {
+                // tree.add(a+b);
+                // if(t>=k){
+                    tree.add(a+b);
+                    if(tree.size()>k){
+                        int temp=tree.poll();
+                        min.add(temp);
+                    }
+                    if(tree.size()<k){
+                        ans[i]=-1;
+                    }
+                    else {
+                        ans[i]=tree.peek();
+                    }
+                // System.out.println(arr);
+
+                // // }
+                // Collections.sort(arr);
+                // // ArrayList<Integer> arr=new ArrayList<Integer>(tree);
+                // if(arr.size()>=k)ans[i]=arr.get(k-1);
+                // else ans[i]=-1;
+               // t++;
+
+            }
+                
+            // }
+            // else ans[i]=-1;
+           // t++;
+        
+        return ans; 
+    }
+}
