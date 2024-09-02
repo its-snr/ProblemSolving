b0cbf02ad078fd1490a994acc5263883fec88711
its-snr <learning.nirmalraj@gmail.com>
2024-09-02 21:41:36 +0530

    Time: 0 ms (100%), Space: 60.2 MB (20.62%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.java
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.java
@@ -1,16 +1,15 @@
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
-
-        int i=0;
-        int n=chalk.length;
-        if(n==1)return 0;
-        while(k>=0){
-            k=k-chalk[i];
-            if(k<0)return i;
-            i++;
-            if(i>=n)i=i%n;
+        return getChalkReplacer(chalk, k);
+    }
+    public int getChalkReplacer(int[] chalk, int remaining_chalk) {
+        int chalk_sum = 0;
+        for (int i = 0; i < chalk.length; i++) {
+            chalk_sum += chalk[i];
+            if (remaining_chalk - chalk_sum < 0) {
+                return i;
+            }
         }
-        return i-1;
-        
+        return getChalkReplacer(chalk, (int) (remaining_chalk % chalk_sum));
     }
 }
