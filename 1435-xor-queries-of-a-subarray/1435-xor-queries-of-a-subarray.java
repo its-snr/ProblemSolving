f1e741ee19440fc8cb697b2b5ccc855c527d88fe
its-snr <learning.nirmalraj@gmail.com>
2024-09-13 21:14:42 +0530

    Time: 2 ms (100%), Space: 57.8 MB (38.53%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.java
@@ -4,19 +4,22 @@ class Solution {
         //System.out.println(n);
 
         int[] ans=new int[n];
+        int[] prefix=new int[arr.length];
+        prefix[0]=arr[0];
+        for(int i=1;i<arr.length;i++){
+            prefix[i]=prefix[i-1]^arr[i];
+
+        }
         
         for(int i=0;i<n;i++){
             int start=queries[i][0];
             int end=queries[i][1];
-            int temp=arr[start];
-            // System.out.println(start);
-            // System.out.println(end);
-            for(int j=start+1;j<=end;j++){
-
-                temp=temp^arr[j];
-               // System.out.println(temp);
+            if(start==0){
+                ans[i]=prefix[end];
             }
-            ans[i]=temp;
+            else{
+                ans[i]=prefix[start-1]^prefix[end];
+            }
         }
         return ans;
         
