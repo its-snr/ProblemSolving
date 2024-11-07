a59ac20e0e8a9f0edb8d38a271033aca832ba0a3
its-snr <learning.nirmalraj@gmail.com>
2024-11-07 18:33:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Split array in three equal sum subarrays/split-array-in-three-equal-sum-subarrays.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCases = Integer.parseInt(br.readLine().trim());
+
+        while (testCases-- > 0) {
+            String[] str = br.readLine().trim().split(" ");
+            int[] arr = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            Solution ob = new Solution();
+            List<Integer> result = ob.findSplit(arr);
+
+            if (result.get(0) == -1 && result.get(1) == -1 || result.size() != 2) {
+                System.out.println("false");
+            } else {
+                int sum1 = 0, sum2 = 0, sum3 = 0;
+                for (int i = 0; i < arr.length; i++) {
+                    if (i <= result.get(0))
+                        sum1 += arr[i];
+
+                    else if (i <= result.get(1))
+                        sum2 += arr[i];
+
+                    else
+                        sum3 += arr[i];
+                }
+                if (sum1 == sum2 && sum2 == sum3) {
+                    System.out.println("true");
+                } else {
+                    System.out.println("false");
+                }
+            }
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+
+    public List<Integer> findSplit(int[] arr) {
+        // Return an array of possible answer, driver code will judge and return true or
+        // false based on
+        int size=arr.length;
+        int arraySum=0;
+        List<Integer>ans=new ArrayList<Integer>();
+        for(int i=0;i<size;i++){
+            arraySum+=arr[i];
+        }
+        if(arraySum%3!=0)return Arrays.asList(-1,-1);
+        
+            int subSum=0;
+            for(int i=0;i<size;i++){
+                subSum+=arr[i];
+                if(subSum==arraySum/3){
+                    ans.add(i);
+                    subSum=0;
+                    if(ans.size()==2)return ans;
+                    
+                }
+            }
+            return Arrays.asList(-1,-1);
+            
+        
+    }
+}
