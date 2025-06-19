939fbd43b6e362b1c5eaceeca98425ec1a89c246
Nirmalraj S <learning.nirmalraj@gmail.com>
2025-06-19 15:09:32 +0530

    Time: 3 ms (100%), Space: 60.3 MB (8.16%) - LeetHub

--- 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.java
+++ 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.java
@@ -1,18 +1,19 @@
 class Solution {
     public int partitionArray(int[] nums, int k) {
 
-        Arrays.sort(nums);
-        int minVal=nums[0];
-        int subsequenceCount=1;
-        for(int i=1;i<nums.length;i++){
-
-            if(nums[i]-minVal>k){
+        int max=0;
+        int subsequenceCount=0;
+        for(int num:nums)max=Math.max(num,max);
+        int[] freq=new int[max+1];
+        for(int num:nums){
+            freq[num]++;
+        }
+        for(int i=0;i<=max;i++){
+            if(freq[i]>0){
                 subsequenceCount++;
-                minVal=nums[i];
+                i+=k;
             }
-
         }
         return subsequenceCount;
-        
     }
 }
