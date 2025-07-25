3f141059b55103e1ba13591970bb8550b72f4cf8
Nirmalraj S <learning.nirmalraj@gmail.com>
2025-07-25 21:42:41 +0530

    Time: 1 ms (100%), Space: 42.5 MB (64.69%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
@@ -2,11 +2,11 @@ class Solution {
     public int maxSum(int[] nums) {
 
         int[] arr=new int[101];
-        int n=nums.length;
+        
         int sum=0;
         int max=Integer.MIN_VALUE;
-        if(n==1)return nums[0];
-        for(int i=0;i<n;i++){
+        if(nums.length==1)return nums[0];
+        for(int i=0;i<nums.length;i++){
             if(nums[i]>0 &&arr[nums[i]]==0){
             sum+=nums[i];
             arr[nums[i]]++;
